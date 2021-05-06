0621cef4f0027538663a55d8c3096eadc1b27a4c
Nicholas Pollock <npollock@catalyte.io>
2021-05-06 15:28:19 -0400

    add cropping

--- edit_image.py
+++ edit_image.py
@@ -3,26 +3,24 @@ from cv2 import cv2, countNonZero, cvtColor
 import numpy as np
 
 def mask_non_colored_image():
-    img1 = Image.open(r"C:/Users/Work/Git/naruto_colored_translated/non_color/Naruto v01/Naruto v1-032.jpg")
-    img2 = Image.open(r"C:/Users/Work/Git/naruto_colored_translated/Naruto color v01-10/Vol 1/00032.jpg")
+    img1 = Image.open(r"C:/Users/Work/Git/naruto_colored_translated/non_color/Naruto v01/Naruto v1-022.jpg")
+    img2 = Image.open(r"C:/Users/Work/Git/naruto_colored_translated/Naruto color v01-10/Vol 1/00022.jpg")
     if img2.size[0]/img2.size[1] < 0.7:
         resized_im_1 = img1.resize((780, 1200))
-        print(resized_im_1.size[0], resized_im_1.size[1 ])
         resized_im_2 = img2.resize((780, 1200))
         resized_im_1 = crop_left_right(resized_im_1)
         resized_im_2 = crop_left_right_rgb(resized_im_2)
+        resized_im_1 = resized_im_1.resize((780, 1200))
+        resized_im_2 = resized_im_2.resize((780, 1200))
         resized_im_2.save('img2.jpg')
         image_data_1 = resized_im_1.load()
         masked_image = resized_im_2
         masked_image_data = masked_image.load()
-        #masked_image = masked_image.transform(masked_image.size, Image.AFFINE, (1, 0, -22, 0, 1, 2))
-        for width in range(resized_im_2.size[0]):
-            for height in range(resized_im_2.size[1]):
+        for width in range(masked_image.size[0]):
+            for height in range(masked_image.size[1]):
                 r,g,b = masked_image_data[width, height]
                 if (r == b and b == g) or (r > 252 and g > 252 and b > 252):
-                    clamph = clamp(height, 0, resized_im_1.height - 1)
-                    clampw = clamp(width, 0, resized_im_1.width - 1)
-                    w = image_data_1[clampw, clamph]
+                    w = image_data_1[width, height]
                     masked_image_data[width, height] = w,w,w
         resized_im_1.save('img1.jpg')
         
@@ -31,68 +29,113 @@ def mask_non_colored_image():
 def crop_left_right(img):
     left_to_remove = 0
     img_data = img.load()
-    last_line = False
+   
     for width in range(img.size[0]):
-        if last_line:
-            break
-        else:
+        w = 0
+        for height in range(img.size[1]):
+            if width == 0 and img_data[width, img.size[1]-height - 1] != 255:
+                break
+            w +=  img_data[width, height]
+        if w < 283333:
             left_to_remove = width
-        for height in range(img.size[1]):
-            w = img_data[width, height]
-            if w < 20  :
-                last_line = True
-                break
-            
-    right_to_remove = 0
-    last_line = False
-    for width in range(img.size[0]):
-        if last_line:
-            print(img.size[0]-width - 1, img.size[1]-height - 1)
-            print(img_data[img.size[0]-width - 1, img.size[1]-height - 1])
             break
-        else:
-            right_to_remove = width
+
+
+    right_to_remove = 0
+    for width in range(img.size[0]):
+        w = 0
         for height in range(img.size[1]):
-            w = img_data[img.size[0]-width - 1, img.size[1]-height - 1]
-            if w < 20 :
-                last_line = True
+            w +=  img_data[img.size[0]-width - 1, img.size[1]-height - 1]
+            if width == 0 and img_data[img.size[0]-width - 1, img.size[1]-height - 1] != 255:
                 break
-    print (right_to_remove)
-    img = img.crop((left_to_remove, 0, img.width, img.height))
-    img = img.crop((0, 0, img.width - right_to_remove, img.height)) 
+        if w < 283333:
+            right_to_remove = width
+            break
+
+    top_to_remove = 0
+    for height in range(img.size[1]):
+        w = 0
+        for width in range(img.size[0]):
+            w +=  img_data[width, height]
+            if height == 0 and img_data[width, height] != 255:
+                print(img_data[width, height])
+                break
+        if w < 185000:
+            top_to_remove = height
+            break
+
+
+    bottom_to_remove = 0
+    for height in range(img.size[1]):
+        w = 0
+        for width in range(img.size[0]):
+            w += img_data[width, img.size[1]-height - 1]
+            if height == 0 and img_data[width, img.size[1]-height - 1] != 255:
+                break
+        if w < 185000:
+            bottom_to_remove = height
+            break
+
+
+    img = img.crop((left_to_remove, 0, img.width, img.height-bottom_to_remove))
+    img = img.crop((0, top_to_remove, img.width - right_to_remove, img.height))
+    print(img.size)
     return img
 
 def crop_left_right_rgb(img):
     left_to_remove = 0
-    img_data = img.convert("RGB").load()
-    last_line = False
+    img_data = img.load()
+   
     for width in range(img.size[0]):
-        if last_line:
-            break
-        else:
-            left_to_remove = width + 6
+        w = 0
         for height in range(img.size[1]):
             r,g,b = img_data[width, height]
-            if not(r == b and b == g):
-                print (r,g,b)
-                last_line = True
+            w += (r + g + b)
+            if height == 0 and (r + g + b) != 765:
                 break
+        if w < 900000:
+            left_to_remove = width
+            break
 
 
     right_to_remove = 0
-    last_line = False
     for width in range(img.size[0]):
-        if last_line:
-            break
-        else:
-            right_to_remove = width
+        w = 0
         for height in range(img.size[1]):
             r,g,b = img_data[img.size[0]-width - 1, img.size[1]-height - 1]
-            if not(r == b and b == g ):
-                last_line = True
+            w += (r + g + b)
+            if width == 0 and (r + g + b) != 765:
                 break
-    img = img.crop((left_to_remove, 0, img.width, img.height))
-    img = img.crop((0, 0, img.width - right_to_remove, img.height)) 
+        print(w)
+        if w < 900000:
+            right_to_remove = width
+            break
+
+    top_to_remove = 0
+    for height in range(img.size[1]):
+        w = 0
+        for width in range(img.size[0]):
+            r,g,b = img_data[width, height]
+            w += (r + g + b)
+        if w < 450000:
+            top_to_remove = height
+            break
+        
+
+
+    bottom_to_remove = 0
+    for height in range(img.size[1]):
+        w = 0
+        for width in range(img.size[0]):
+            r,g,b = img_data[width, img.size[1]-height - 1]
+            w += (r + g + b)
+        if w < 450000:
+            bottom_to_remove = height
+            break
+
+
+    img = img.crop((left_to_remove, 0, img.width, img.height-bottom_to_remove))
+    img = img.crop((0, top_to_remove, img.width - right_to_remove, img.height)) 
     return img
 
 def clamp(num, min_value, max_value):
